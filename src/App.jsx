2d4738995549f5226ff7fa3742278a5831618d6b
EsystemICT <esystemict@esystem.com.my>
2026-02-12 16:29:00 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -281,7 +281,7 @@ export default function App() {
       <div className="app-container">
         <div className="login-container">
           <form className="login-card" onSubmit={handleLogin}>
-            <h1><i className="fa-solid fa-hotel"></i> Aladdin Hotel</h1>
+            <h1><i className="fa-solid fa-hotel"></i> Aladdin Dream Hotel</h1>
             <h3 style={{color:'#666', marginBottom:'20px'}}>Staff Login</h3>
             <input placeholder="User ID" value={loginId} onChange={e => setLoginId(e.target.value)} required />
             <input type="password" placeholder="Password" value={loginPass} onChange={e => setLoginPass(e.target.value)} required />
@@ -299,7 +299,7 @@ export default function App() {
       <header className="header">
         <div className="header-content">
           <h1>
-             Aladdin Hotel
+             Aladdin Dream Hotel
              <div className="user-profile" onClick={() => setShowPasswordModal(true)}>
                <i className="fa-solid fa-circle-user" style={{color: '#ddbd88'}}></i>
                <span style={{fontWeight: 'bold'}}>{currentUser.name}</span>

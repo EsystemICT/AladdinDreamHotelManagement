f38806672fcd2d96501c38c9cd6f21d200f96797
EsystemICT <esystemict@esystem.com.my>
2026-02-13 15:33:09 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { db } from './firebase'; 
+import { db } from './firebase';
 import { collection, onSnapshot, doc, updateDoc, addDoc, deleteDoc, serverTimestamp, query, orderBy, where, getDocs, limit, writeBatch } from 'firebase/firestore';
 import './App.css';
 import * as XLSX from 'xlsx';
